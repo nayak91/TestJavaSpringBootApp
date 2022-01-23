2e838b1378131e44230812b24ee686d617113e2d
Ubuntu <ubuntu@ip-172-31-34-82.ap-northeast-1.compute.internal>
2022-01-23 10:30:06 +0000

    test

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "install dependencies"
+sudo apt update
+sudo apt install default-jdk
+sudo apt install maven
 cd /home/ubuntu/codebase/TestJavaSpringBootApp
 echo pwd:$(pwd)
 sudo mvn clean install
