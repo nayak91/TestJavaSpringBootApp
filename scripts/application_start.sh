21199ed4b974e91173971c16b7fc6d341537815b
Ubuntu <ubuntu@ip-172-31-34-82.ap-northeast-1.compute.internal>
2022-01-23 12:21:31 +0000

    test1

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "starting server started"
 cd /home/ubuntu/codebase/TestJavaSpringBootApp/target
-echo pwd:$(pwd)
-sudo nohup java -jar target/SpringBoot-0.0.1-SNAPSHOT.jar > test.log &
+echo application_start file - pwd:$(pwd)
+sudo nohup java -jar SpringBoot-0.0.1-SNAPSHOT.jar > /home/ubuntu/myapp.log &
 echo "startig server finished"
