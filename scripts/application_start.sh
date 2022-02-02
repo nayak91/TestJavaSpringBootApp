a17be1adfe43510048d9ce57563312b1f2c44403
Ubuntu <ubuntu@ip-172-31-34-82.ap-northeast-1.compute.internal>
2022-02-02 05:31:20 +0000

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "starting server started"
-cd /home/ubuntu/codebase/TestJavaSpringBootApp/target
+cd /home/ubuntu/codebase/TestSpringApp
 echo application_start file - pwd:$(pwd)
 sudo nohup java -jar SpringBoot-0.0.1-SNAPSHOT.jar > /home/ubuntu/myapp.log &
 echo "startig server finished"
--- scripts/create_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "create build sh file called"
-echo install_dependencies file1- pwd:$(pwd)
-cd /home/ubuntu/codebase/TestJavaSpringBootApp
-echo install_dependencies file2- pwd:$(pwd)
-sudo mvn clean install
-echo "create build sh file finished"
